f9ddc33c3b17a2f3391b034e262a8732e6468770
songyu <songyu@node0.22.kkprojects-pg0.utah.cloudlab.us>
2024-09-07 01:27:59 -0600

    FIX: fix mr bug

--- src/RDMA_utils.c
+++ src/RDMA_utils.c
@@ -75,21 +75,24 @@ int rdma_init()
     log_debug("local mr_size: %u", rparams.local_mr_size);
     log_debug("remote mr_size: %u", rparams.remote_mr_size);
 
-    cfg->local_mempool_addrs = (void **)calloc(rparams.local_mr_num, sizeof(void *));
+    cfg->local_mempool_addrs = (void **)calloc(cfg->local_mempool_size, sizeof(void *));
     if (!cfg->local_mempool_addrs)
     {
         log_error("failed to allocate local_mempool_addrs");
         goto error;
     }
-    cfg->remote_mempool_addrs = (void **)calloc(rparams.remote_mr_num, sizeof(void *));
-    if (!cfg->local_mempool_addrs)
-    {
-        log_error("failed to allocate local_mempool_addrs");
-        goto error;
-    }
-
     retrieve_mempool_addresses(cfg->mempool, cfg->local_mempool_addrs);
-    retrieve_mempool_addresses(cfg->remote_mempool, cfg->remote_mempool_addrs);
+
+    if (cfg->use_one_side == 1) {
+        cfg->remote_mempool_addrs = (void **)calloc(cfg->remote_mempool_size, sizeof(void *));
+        if (!cfg->remote_mempool_addrs)
+        {
+            log_error("failed to allocate local_mempool_addrs");
+            goto error;
+        }
+
+        retrieve_mempool_addresses(cfg->remote_mempool, cfg->remote_mempool_addrs);
+    }
 
     log_info("init RDMA ctx");
 
@@ -122,9 +125,18 @@ int rdma_init()
         log_error("failed to allocate mp_elt_to_mr_map");
         goto error;
     }
-    for (size_t i = 0; i < rparams.local_mr_num; i++)
-    {
-        g_hash_table_insert(cfg->mp_elt_to_mr_map, (gpointer)cfg->local_mempool_addrs[i], cfg->rdma_ctx.local_mrs[i]);
+    if (cfg->use_one_side == 1) {
+        for (size_t i = 0; i < rparams.local_mr_num; i++)
+        {
+            g_hash_table_insert(cfg->mp_elt_to_mr_map, (gpointer)cfg->local_mempool_addrs[i], cfg->rdma_ctx.local_mrs[i]);
+        }
+    }
+    if (cfg->use_one_side == 0) {
+        for (size_t i = 0; i < rparams.remote_mr_num; i++)
+        {
+            g_hash_table_insert(cfg->mp_elt_to_mr_map, (gpointer)cfg->local_mempool_addrs[i], cfg->rdma_ctx.remote_mrs[i]);
+        }
+
     }
 
     cfg->node_res = (struct rdma_node_res *)calloc(cfg->n_nodes, sizeof(struct rdma_node_res));
