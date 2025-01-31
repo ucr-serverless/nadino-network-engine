994a0e20c9b8bb6cfebea2f3d7a2420eabd9a216
songyu <localhost>
2025-01-30 22:06:04 -0800

    fix data len

--- src/gateway.cpp
+++ src/gateway.cpp
@@ -967,7 +967,7 @@ static int server_init(struct server_vars *sv)
             }
             else {
                 // TODO: create from raw ptr
-                result = create_doca_bufs(g_ctx, i.first, sizeof(struct http_transaction*), i.second.element_raw_ptr.get(), i.second.n_element_raw_ptr);
+                result = create_doca_bufs(g_ctx, i.first, sizeof(struct http_transaction), i.second.element_raw_ptr.get(), i.second.n_element_raw_ptr);
                 log_info("buf range: %d", sizeof(struct http_transaction*));
                 i.second.rr_element_addr.reserve(g_ctx->rr_per_ctx);
                 uint64_t min_sz = std::min((uint64_t)g_ctx->rr_per_ctx, i.second.n_receive_pool_element_raw_ptr);
